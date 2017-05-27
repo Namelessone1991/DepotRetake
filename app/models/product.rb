a3ec8e6eb29831dcadf5b71fa009771579d3c014
nameless <Ernesto.Roldan.Rh@gmail.com>
2017-05-27 14:29:39 -0500

    Finished chapter 9

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+
+    has_many :line_items
+
+    before_destroy :ensure_not_referenced_by_any_line_item
      
     validates :title, :description, :image_url, presence: true
 
@@ -17,5 +21,17 @@ class Product < ApplicationRecord
               message: 'must be a URL for GIF, JPG or PNG image.'
               }
 
+
+      private
+
+      def ensure_not_referenced_by_any_line_item
+
+           unless line_items.empty?
+
+           errors.add(:base,'Line items present')
+           throw :abort
+           end   
+      end
+    
 
 end
