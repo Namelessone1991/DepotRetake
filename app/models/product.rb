74e49fbc1ea80e0dbfb0774020b9cfce7943b806
nameless <Ernesto.Roldan.Rh@gmail.com>
2017-05-26 19:16:16 -0500

    Added validation test functions do not work start of chapter 8 page 101

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,21 @@
 class Product < ApplicationRecord
+     
+    validates :title, :description, :image_url, presence: true
+
+
+    #This helper validates that your attributes have only numeric values. 
+    #By default, it will match an optional sign followed by an integral or floating point number. 
+    #To specify that only integral numbers 
+   
+    validates :price, numericality: {greater_than_or_equal_to: 0.01}    
+
+    validates :title, uniqueness: true
+
+
+    validates :image_url, allow_blank: true, format: {
+              with: %r{\.(gif|jpg|png)\Z}i,
+              message: 'must be a URL for GIF, JPG or PNG image.'
+              }
+
+
 end
